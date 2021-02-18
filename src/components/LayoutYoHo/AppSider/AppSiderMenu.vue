7f85f40d865df7458d75439f699589755f38e695
Carlos <i@carlos.com.cn>
2021-02-18 22:45:28 +0800

    feat: 新增 侧边栏菜单 组件, 支持图标, 简单实现

---
+++ src/components/LayoutYoHo/AppSider/AppSiderMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="app-sider-menu">
+    <ul class="">
+      <template v-for="(item, i) in model" :key="item.label + i">
+        <li class="app-sider-menu-item" :class="item.class">
+          <router-link :to="item.to" class="app-sider-menu-link">
+            <span class="app-sider-menu-icon"><CIconPark :icon="item.icon" size="1.25rem" /></span>
+            <span class="app-sider-menu-text">{{ item.label }}</span>
+          </router-link>
+        </li>
+      </template>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import CIconPark from '@/components/common/CIconPark'
+
+export default defineComponent({
+  name: 'AppSiderMenu',
+  components: { CIconPark },
+  props: {
+    model: {
+      type: Array,
+      default: null,
+    },
+  },
+  setup(props) {
+    const menuItems = computed(() => props.model)
+    return {}
+  },
+})
+</script>
